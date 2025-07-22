497cc66037b537bf4902619830eb58ecff501088
Sinashahbeyk <Sinashahbeyk@gmail.com>
2025-07-22 17:04:34 +0330

    change run_once to daily_monitor

--- scheduler.py
+++ scheduler.py
@@ -23,8 +23,8 @@ async def save_if_not_exists(repo, keyword):
         db.commit()
     db.close()
 
-async def run_once():
-    since = (datetime.utcnow() - timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%SZ")
+async def daily_monitor():
+    since = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%SZ")
     for kw in MONITORED_KEYWORDS:
         for lang in MONITORED_LANGUAGES:
             repos = await github_monitor(kw, lang, since)
@@ -40,30 +40,4 @@ async def run_once():
 
 
 if __name__ == "__main__":
-    asyncio.run(run_once())
-
-
-
-# async def daily_monitor():
-#     since = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%dT%H:%M:%SZ")
-#     for kw in MONITORED_KEYWORDS:
-#         for lang in MONITORED_LANGUAGES:
-#             repos = await github_monitor(kw, lang, since)
-#             matched = [r for r in repos if match_exact_keyword(r, kw)]
-
-#             folder = "github_monitor"
-#             os.makedirs(folder, exist_ok=True)
-#             fname = f"{since.split('T')[0]}_{kw}_{lang}.json"
-#             with open(os.path.join(folder, fname), "w") as f:
-#                 json.dump(matched, f, indent=2)
-
-#             await asyncio.gather(*(save_if_not_exists(r, kw) for r in matched))
-
-# async def run_scheduler():
-#     while True:
-#         await daily_monitor()
-#         await asyncio.sleep(86400)
-
-# def start_scheduler():
-#     loop = asyncio.get_event_loop()
-#     loop.create_task(run_scheduler())
+    asyncio.run(daily_monitor())
